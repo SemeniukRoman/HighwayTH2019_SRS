e6eb1d7227d1b91e68e1da86e0b941fbdb186beb
SemeniukRoman <SemeniukRomanSer@gmail.com>
2019-04-11 15:04:21 +0300

    test commit

--- src/main/java/lesson4/HomeWork4.java
+++ src/main/java/lesson4/HomeWork4.java
@@ -10,14 +10,11 @@ public class HomeWork4 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s1 = reader.readLine();
         String s2 = reader.readLine();
+        String sameStr = s1;
 
         // Сравнение имен
         if(s1.equals(s2)){
-            System.out.println("Имена идентичны");
-        } else if(s1 != s2){
-            if(s1.length() == s2.length()){
-                System.out.println("Длины имен равны");
+            System.out.println("Тезки");
+        } else System.out.println("Имена разные");
             }
         }
-    }
-}
