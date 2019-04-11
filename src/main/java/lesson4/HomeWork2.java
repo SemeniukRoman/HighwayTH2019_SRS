725965d94cbf0ccbf5b75a11fe49af5a9d66bdf0
SemeniukRoman <SemeniukRomanSer@gmail.com>
2019-04-11 15:05:47 +0300

    test commit

--- src/main/java/lesson4/HomeWork2.java
+++ src/main/java/lesson4/HomeWork2.java
@@ -10,7 +10,8 @@ public class HomeWork2 {
         int b = 0;
         int c = 0;
         int d = 0;
-        int count = 1;
+        int max =0;
+        int count = 0;
 
         Scanner in = new Scanner(System.in);
 
@@ -23,8 +24,12 @@ public class HomeWork2 {
         System.out.print("Print d: ");
         d = in.nextInt();
 
-        if (a==b||a==c||a==d||b==c||b==d||c==d) count=count++;
-        else count=count;
+        if (a > b && a > c && a > d ) max = a;
+        else if (b > a && b > c && b > d ) max = b;
+        else if (c > a && c > b && c > d ) max = c;
+        else if (d > a && d > c && d > b ) max = d;
+        System.out.println("The biggest number: " + max);
+       if (a==b||a==c||a==d||b==c||c==d) count=count++;
         System.out.println("Count biggest numder: " + count);
     }
     }
